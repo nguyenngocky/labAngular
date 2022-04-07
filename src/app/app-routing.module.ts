f5c95190ac0cf816d0efd3cd563104fecc94cdea
nguyenngocky <nguyenngocky1995@gmail.com>
2022-04-07 21:42:10 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeAdminPageComponent } from './view/admin/home-admin-page/home-admin-page.component';
+import { HomePageComponent } from './view/client/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomePageComponent,
+    children: [
+      {
+        path: "",
+        component: HomePageComponent,
+      },
+    ]
+  },
+  {
+    path: "admin",
+    component: HomeAdminPageComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: 'home-admin',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home-admin',
+        component: HomeAdminPageComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import { HomeAdminPageComponent } from './view/admin/home-admin-page/home-admin-page.component';
+import { HomePageComponent } from './view/client/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HomeAdminPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ToastrModule.forRoot(),
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
